62cb83a8b6faf4e1dafd5ff81df51cc2fa32ca81
SelvaKumar T <34051209+imsk003@users.noreply.github.com>
2019-02-21 01:08:22 +0530

    Add files via upload

---
+++ customer-tracker/src/main/java/sk/learning/hibernate/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package sk.learning.hibernate.dao;
+
+import java.util.List;
+
+import sk.learning.hibernate.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
---
+++ customer-tracker/src/main/java/sk/learning/hibernate/service/CustomerService.java
@@ -0,0 +1,17 @@
+package sk.learning.hibernate.service;
+
+import java.util.List;
+
+import sk.learning.hibernate.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
